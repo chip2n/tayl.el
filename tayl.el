6d26d3b99adf20978c8fa6512d9ff73557e612a9
Andreas Arvidsson <andreas.arvidson@gmail.com>
2019-02-24 16:25:56 +0100

    Add license and library header

--- tayl.el
+++ tayl.el
@@ -1,3 +1,27 @@
+;;; tayl.el --- Tayl client for Emacs
+
+;; Copyright (C) 2019 Andreas Arvidsson
+
+;; Author: Andreas Arvidsson <andreas@arvidsson.io>
+;; Keywords: tools
+;; Version: 0.0.1
+;; Package-Requires: ((request "0.3.0"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
 (require 'request)
 
 (defvar tayl-api-token nil)
@@ -37,3 +61,5 @@
     (elfeed-get-url-at-point)))
 
 (provide 'tayl)
+
+;;; tayl.el ends here
